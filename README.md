20b45f74c4e96e69f8bf90e88a361d792b050156
SquishyStrawberry <therealrebuilder@gmail.com>
2015-11-08 03:26:33 +0100

    Command events

---
+++ README.md
@@ -0,0 +1,9 @@
+# irc-nodejs-framework
+
+A framework for the IRC Protocol in NodeJS
+
+# TODO
+
+1. Support for multiple channels
+2. More!
+
--- example.js
+++ example.js
@@ -2,5 +2,14 @@ var irc = require("./lib/main.js");
 var host = "irc.editingarchive.com",
     port = 6667;
 
-var bot = new irc.IRCBot(host, port, "node_framework_test");
+var bot = new irc.IRCBot(host, port, 
+                         "node_framework_test", "#bottest");
+
+bot.on("join", function(e) {
+    this.sendMessage("Hey " + (e.nick === this.nick ? "" : e.nick) + "!");
+});
+
+bot.on("commandTerminate", function(commandInfo) {
+    bot.quit("Goodbye");
+});
 
