6b7c5f9de6cf2876dbbf300c739382a69232481f
SquishyStrawberry <therealrebuilder@gmail.com>
2015-11-08 01:30:44 +0100

    Basic connection, joins channel but doesn't do anything after that.

---
+++ example.js
@@ -0,0 +1,6 @@
+var irc = require("./lib/main.js");
+var host = "irc.editingarchive.com",
+    port = 6667;
+
+var bot = new irc.IRCBot(host, port, "node_framework_test");
+
--- src/irc_utils.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    stripColor: function stripColor(str) {
-        // FIXME This obviously doesn't strip cpolors
-        return str;
-    }
-}
-
