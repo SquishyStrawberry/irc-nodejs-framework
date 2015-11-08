ca2767ab17f1e2de4ac07946b12026bb686208fd
SquishyStrawberry <therealrebuilder@gmail.com>
2015-11-08 03:31:59 +0100

    Make examples its own dir

---
+++ examples/terminate.js
@@ -0,0 +1,21 @@
+var irc = require("./lib/main.js"),
+    fs = require("fs");
+
+var host = "irc.editingarchive.com",
+    port = 6667;
+
+fs.readFile("host.json", function(data) {
+    
+    var info = JSON.parse(data);
+    var bot = new irc.IRCBot(info.host, info.port,
+                             info.nick, info.channel);
+
+    bot.on("join", function(e) {
+        this.sendMessage("Hey " + (e.nick === this.nick ? "" : e.nick) + "!");
+    });
+
+    bot.on("commandTerminate", function(commandInfo) {
+        bot.quit("Goodbye");
+    });
+});
+
